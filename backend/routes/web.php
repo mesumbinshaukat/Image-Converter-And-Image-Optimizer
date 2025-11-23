0bfc8b6365b63ea80c7fa6f5868c4b866d898ca4
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2025-11-24 04:24:26 +0500

    Fix: Remove Laravel welcome route to serve React app, add backend .htaccess

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Removed the default welcome route to allow index.html (React app) to be served
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// All frontend routes are handled by the React app (index.html)
+// API routes are in routes/api.php
